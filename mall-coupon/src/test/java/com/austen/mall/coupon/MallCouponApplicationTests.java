7ce69c700b772c882ec3a4d67bed878a782f28ac
Austen <923502259@qq.com>
2020-08-16 16:49:14 +0800

    init project

---
+++ mall-coupon/src/test/java/com/austen/mall/coupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.austen.mall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
